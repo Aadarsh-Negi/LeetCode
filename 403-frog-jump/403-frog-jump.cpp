f75d6e63736b0107a3d144e096d310ee9a295359
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-07 10:11:53 -0800

    Time: 240 ms (55.09%), Space: 48.6 MB (44.31%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
-    unordered_map<int,int> cord;
-    int last;
-    map<pair<int,int>,int> dp;
-    bool solve(int i=0,int k=1){
-        
-        if(cord[i+k]==0 || k<=0) return 0;
-        
-        if(i+k == last) return 1;
-        
-        if(dp.count({i,k})) return dp[{i,k}];
-        
-        return dp[{i,k}] = solve(i+k,k) || solve(i+k,k-1) || solve(i+k,k+1);
-        
-//         // if(cord.count(i)==0) return 0;
-//         if(i==last) return 1;
-//         if(i>last || i<0 ) return 0;
-//         if(dp.count({i,k})>0) return dp[{i,k}];
-//         bool ok=0;
-        
-//         if(cord.count(i+k)){
-            
-//             ok|=solve(i+k,k);
-//             // dp[i][k]=ok;
-            
-//         }
-//         if(cord.count(i+k+1)){
-            
-//             ok|=solve(i+k-1,k-1);
-//             // dp[i][k]=ok;
-            
-//         }
-//         if(k-1>0 && cord.count(i+k-1)){
-            
-//             ok|=solve(i+k+1,k+1);
-//             // dp[i][k]=ok;
-            
-//         }
-//         return dp[{i,k}] = ok;
-    
-    }
-    
-    
     bool canCross(vector<int>& stones) {
-        for(int &i:stones) cord[i]=1;
-            last = stones.back();
+        queue<pair<int,int>> qq;
         
+        unordered_map<int,int> ck;
+        for(int &i:stones) ck[i]=1;
         
-        return solve();
+        if(stones[1]!=1) return 0;
+        if(stones.size()==2){
+            if(stones[1]!=1) return 0;
+            return 1;
+        }
+        map<pair<int,int>,int> seen;
+        qq.push({1,1});
+        qq.push({1,2});
+        
+        while(qq.size()){
+            int n=qq.size();
+                
+                while(n){
+                    n--;
+                    int i = qq.front().first;
+                    int k = qq.front().second;
+                    qq.pop();            
+                    if(i+k == stones.back()) return 1;
+                    if(seen[{i,k}]) continue;
+                    seen[{i,k}]=1;
+                    if(ck[i+k]){
+                        if(k)
+                            qq.push({i+k,k});
+                        if(k-1>0)
+                            qq.push({i+k,k-1});
+                        qq.push({i+k,k+1});
+                    }
+                    
+        
+                }
+            
+        }
+        return 0;
+        
         
     }
 };
