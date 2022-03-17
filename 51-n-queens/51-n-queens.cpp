78833ff6072d294ed3df585e4803ddffa2a48e6e
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-17 12:11:47 -0700

    Time: 13 ms (44.81%), Space: 7.8 MB (39.24%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    // int timer=0;
+     set<vector<string>> ans;
+    vector<string> bb;
+    bool allok(int i,int j,int &n){
+        // cout<<timer++<<"\n";
+        // for(int k=0;k<bb.size();k++) cout<<bb[i]<<"<\n";
+        for(int k=0;k<bb.size();k++){
+                if(bb[k][j]=='Q') return 0;
+        }
+        for(int k=0;k<bb[0].size();k++){
+                if(bb[i][k]=='Q') return 0;
+        }
+        
+        for(int k=i-1,k2=j-1;k>=0 && k2>=0;k--,k2--){
+            if(bb[k][k2]=='Q') return 0;
+        }
+        for(int k=i+1,k2=j+1;k<n && k2<n;k++,k2++){
+            if(bb[k][k2]=='Q') return 0;
+        }
+         for(int k=i-1,k2=j+1;k>=0 && k2<n;k--,k2++){
+            if(bb[k][k2]=='Q') return 0;
+        }
+         for(int k=i+1,k2=j-1;k<n && k2>=0;k++,k2--){
+            if(bb[k][k2]=='Q') return 0;
+        }
+       
+        return 1;
+    }
+    void get_all(int &n,int i,int cnt=0){
+        if(i>=n){
+            if(cnt==n)    
+            ans.insert(bb);
+            
+            return;
+        }
+        
+        for(int k=0;k<n;k++){
+            if(allok(i,k,n)){
+                // cout<<"here\n";
+                bb[i][k]='Q';
+                get_all(n,i+1,cnt+1);
+                bb[i][k]='.';
+            }
+        }        
+    }
+    
+    
+    vector<vector<string>> solveNQueens(int n) {
+        string ss(n,'.');
+        for(int i=0;i<n;i++) bb.push_back(ss);
+        get_all(n,0);
+       vector<vector<string>> aa;
+        for(auto it:ans) aa.push_back(it);
+        return aa;
+    }
+};
