978cccb7e2c2a2c0759e73dcd7f46a75a5f30c90
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-25 12:50:32 +0530

    Time: 4 ms (78.75%), Space: 8.4 MB (24.05%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    map<pair<int,int>,int> dp;
-    vector<vector<int>> cc;
-    int solve(int i,int a,int b){
+    vector<vector<int>> dp;
+    
+    int solve(int i,int a,int b,vector<vector<int>> &cc){
         if(i>=cc.size()) return 0;
         
-        if(dp.count({a,b})) return dp[{a,b}];
+        if(dp[a][b]!=-1) return dp[a][b];
         
         int temp = INT_MAX;
         
         if(a){
-            temp =min(temp,cc[i][0] + solve(i+1,a-1,b));
+            temp =min(temp,cc[i][0] + solve(i+1,a-1,b,cc));
         }
         
         if(b){
-            temp =min(temp,cc[i][1] + solve(i+1,a,b-1));
+            temp =min(temp,cc[i][1] + solve(i+1,a,b-1,cc));
         }
         
         
-        return dp[{a,b}]=temp;
+        return dp[a][b]=temp;
     }
     
     
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        cc=costs;
         int a = costs.size()/2;
-         
-        return solve(0,a,a);
+        dp.resize(a+1);
+        for(auto &i:dp) i.resize(a+1),fill(i.begin(),i.end(),-1);
+        
+        return solve(0,a,a,costs);
     }
 };
