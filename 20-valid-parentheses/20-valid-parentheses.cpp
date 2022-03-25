0ca9e749fb12f8354bfa8cf6428c768f702edfc8
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-25 13:58:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (9.84%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> ck;
+     
+        stack<int> st;
         
         
         for(char &c:s){
-          if(c=='(' || c=='{' || c=='[') ck.push(c);
-          else{
-             if(ck.size()==0) return 0;
-             if(ck.top() == '(' and c==')') ck.pop();
-             else if(ck.top() == '[' and c==']') ck.pop();
-             else if(ck.top() == '{' and c=='}') ck.pop();
-             else return 0;
-           }  
-            
+            if(c=='(' || c=='{' || c=='[') st.push(c);
+            else{
+                if(st.size()==0) return 0;
+                
+                if(st.top()=='(' && c==')') st.pop();
+                else if(st.top()=='{' && c=='}') st.pop();
+                else if(st.top()=='[' && c==']') st.pop();
+                else return 0;
+            }
         }
-        if(ck.size()) return 0;
-        return 1;
+        
+        
+        if(st.size()==0) return 1;
+        
+        return 0;
+        
     }
 };
