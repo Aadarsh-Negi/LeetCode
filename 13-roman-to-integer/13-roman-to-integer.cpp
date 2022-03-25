2abea94cfc8c13c10adfe0576e9648aaa44fa420
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-25 13:52:55 +0530

    Time: 18 ms (49.11%), Space: 6 MB (88.48%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
+    
+    int val(char c){
+        if(c=='I') return 1;
+        if(c=='V') return 5;
+        if(c=='X') return 10;
+        if(c=='L') return 50;
+        if(c=='C') return 100;
+        if(c=='D') return 500;
+        return 1000;
+    }
+    
     int romanToInt(string s) {
-        
-        map<char,int> mp;
-        
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
+      
         
         int n = s.size();
         
         int ans=0;
         
-        
-        // I 
-        
-        // cc c n=3
-        //    2
-        
-        // I X X
-        //
-        
+
         
         
        for(int i=0;i<n;i++){
            char cur = s[i];
             if(i+1<n){
                 char nxt = s[i+1];
-                if(mp[cur] >= mp[nxt]){
-                    ans+=mp[cur];
+                if(val(cur) >= val(nxt)){
+                    ans+=val(cur);
                 }else{
-                    ans+= (mp[nxt]-mp[cur]);
+                    ans+= (val(nxt)-val(cur));
                     i++;
                 }
             }else{
-                ans+=mp[cur];
+                ans+=val(cur);
             }   
        }
         return ans;
