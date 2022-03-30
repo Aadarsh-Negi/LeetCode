3179a6dd4d712afd6d8b9a237dabef0fd333f3b1
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-30 09:36:23 +0530

    Time: 8 ms (35.04%), Space: 9.6 MB (5.69%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int tar) {
+        int i=0;
+        int i2=mat.size()-1;
+//         if(mat.size()==1){
+//             int j=0;
+//             int j2=mat[0].size()-1;
+//                 while(j<=j2){
+//                     // cout<<mid<<"<\n";
+//                     int mid2 = j + (j2-j)/2;
+                    
+//                     if(mat[0][mid2]==tar) return 1;
+//                     else if(mat[0][mid2]>tar) j2=mid2-1;
+//                     else j=mid2+1;
+                    
+//                 }
+//                 return 0;
+//         }
+        
+        while(i<=i2){
+            
+            int mid = i + (i2-i)/2;
+            // cout<<mid<<"\n";
+            
+            if(mat[mid][0]==tar || mat[mid][mat[0].size()-1]==tar) return 1;
+            
+            if(mat[mid][0]<=tar && mat[mid][mat[0].size()-1]>=tar){
+                int j=0;
+                int j2=mat[0].size()-1;
+                while(j<=j2){
+                    // cout<<mid<<"<\n";
+                    int mid2 = j + (j2-j)/2;
+                    
+                    if(mat[mid][mid2]==tar) return 1;
+                    else if(mat[mid][mid2]>tar) j2=mid2-1;
+                    else j=mid2+1;
+                    
+                }
+                return 0;
+ 
+            }else{
+                
+                if(mat[mid][0]<tar) i=mid+1;
+                else i2=mid-1;
+                
+            }
+
+        }
+        return 0;
+        
+        
+    }
+};
