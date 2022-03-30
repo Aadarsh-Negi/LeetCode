0a702c88888d26afe0877f3f933d217aa7a42aa3
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-03-30 10:50:29 +0530

    Time: 16 ms (57.47%), Space: 15.8 MB (5.29%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& ar, int tar) {
-        map<int,int> ind;
+        unordered_map<int,int> ind;
         for(int i=0;i<ar.size();i++){
             if(ind[tar-ar[i]]) return {ind[tar-ar[i]],i+1};
             ind[ar[i]]=i+1;
