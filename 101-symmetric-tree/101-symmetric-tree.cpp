c6d541d10159eb0e14b6fc6abe90df8ffa842107
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-01 14:37:26 +0530

    Time: 3 ms (89.98%), Space: 16.5 MB (42.31%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -14,15 +14,11 @@ public:
     bool check(TreeNode *f1,TreeNode *f2){
         if(!f1 && !f2) return 1;
         if(!f1 || !f2) return 0;
-        
         if(f1->val!=f2->val) return 0;
         return check(f1->left,f2->right) && check(f1->right,f2->left);
     }
     bool isSymmetric(TreeNode* root) {
-        if(root->left && root->right){
-           return check(root->left,root->right); 
-        }   
-        if(root->left || root->right) return 0;
-        return 1;
+           return check(root,root); 
+      
     }
 };
