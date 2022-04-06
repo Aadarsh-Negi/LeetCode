269025a908abcc43e97b0d09a5cb8f434f851b89
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-06 08:33:36 +0530

    Time: 547 ms (40.82%), Space: 10.4 MB (90.82%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -3,16 +3,16 @@ public:
     int threeSumMulti(vector<int>& arr, int tar) {
         long long ans=0;
         const int mod = 1e9 + 7;
+        int cnt[305]={0};
+        cnt[arr[0]]++;
         for(int i=1;i<arr.size();i++){
             int tt = tar - arr[i];
-            int cnt[305]={0};
-            for(int j=i-1;j>=0;j--) cnt[arr[j]]++;
-            
             for(int j=i+1;j<arr.size();j++){
                if(tt-arr[j]<0) continue;
                 ans+=cnt[tt-arr[j]];
                 ans%=mod;
             }
+            cnt[arr[i]]++;
             
         }
         return ans;
