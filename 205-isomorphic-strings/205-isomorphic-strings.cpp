35f1554892ad2a898a8499f2c6322dae48658530
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-07 21:17:08 +0530

    Time: 9 ms (52.37%), Space: 7.2 MB (19.05%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool ck(string s,string t){
-        int cnt[405];
+        int cnt[200];
         memset(cnt,-1,sizeof(cnt));
    
     for(int i=0;i<s.size();i++){
-        if(cnt[s[i]]!=-1){
-            if(cnt[s[i]]!=t[i]) return false;
-        }
-       else  cnt[s[i]]=t[i];
+        if(cnt[s[i]]!=-1)
+            if(cnt[s[i]]!=t[i]) return 0;
+        cnt[s[i]]=t[i];
     }
-    return true;
+    return 1;
 }
     bool isIsomorphic(string s, string t) {
       
