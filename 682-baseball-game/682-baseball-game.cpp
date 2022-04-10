e44368d422dc7889871ae2ef53c36aa135a42f88
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-10 09:22:56 +0530

    Time: 4 ms (71.34%), Space: 8.5 MB (28.60%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
-        stack<string> stc;
+        stack<int> stc;
         
         for(string &s:ops){
-            if(s=="D"){
-                int prev = stoi(stc.top()) * 2;
-                stc.push(to_string(prev));
-            }else if(s=="C") stc.pop();
+            if(s=="D") stc.push(stc.top()*2);
+            else if(s=="C") stc.pop();
             else if(s=="+") {
-                string x  = stc.top(); stc.pop();
-                string y = stc.top();
+                int  x = stc.top(); stc.pop();
+                int y = stc.top();
                 stc.push(x);
-                stc.push(to_string(stoi(x) + stoi(y)));
-            }else stc.push(s);
+                stc.push(x + y);
+            }else stc.push(stoi(s));
         }
         int ans=0;
-        while(stc.size()) ans+=stoi(stc.top()),stc.pop();
+        while(stc.size()) ans+=stc.top(),stc.pop();
         return ans;
         
     }
