a18ce668b48c7ca2750589d4abf07cdba9d42b24
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-15 10:48:41 +0530

    Time: 95 ms (31.95%), Space: 32.1 MB (33.09%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        map<int,int> vis;
+        unordered_map<int,int> vis;
         queue<pair<int,int>> qq;
-        set<int> fr;
-        for(int &i:forbidden) fr.insert(i);
+        unordered_map<int,int> fr;
+        for(int &i:forbidden) fr[i]=1;
         qq.push({0,0});
         int ans=0;
         
@@ -15,11 +15,11 @@ public:
                 int cur = qq.front().first;
                 int back = qq.front().second;
                 qq.pop();
-                if(vis[cur] || cur>9999 || cur<-9999) continue;
+                if(vis[cur] || cur>9999) continue;
                 vis[cur]=1;
                 if(cur==x) return ans;
-                if(fr.count(cur-b)==0 && back==0 && cur-b>=0) qq.push({cur-b,1});
-                if(fr.count(cur+a)==0) qq.push({cur+a,0});      
+                if(fr[cur-b]==0 && back==0 && cur-b>=0) qq.push({cur-b,1});
+                if(fr[cur+a]==0) qq.push({cur+a,0});      
                 
             }
             ans++;
