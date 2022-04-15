ceaa0b6de7eed281ad9ac8662ec3898c121ac14e
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-15 10:52:33 +0530

    Time: 16 ms (95.35%), Space: 13.1 MB (92.57%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -3,8 +3,8 @@ public:
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
         int vis[7777]={0};
         queue<pair<int,int>> qq;
-        int fr[7777] = {0};
-        for(int &i:forbidden) fr[i]=1;
+        
+        for(int &i:forbidden) vis[i]=1;
         qq.push({0,0});
         int ans=0;
         
@@ -18,8 +18,8 @@ public:
                 if(vis[cur] || cur>=7777) continue;
                 vis[cur]=1;
                 if(cur==x) return ans;
-                if(cur-b>=0 && fr[cur-b]==0 && back==0) qq.push({cur-b,1});
-                if(cur+a<7777 && fr[cur+a]==0) qq.push({cur+a,0});      
+                if(cur-b>=0 && vis[cur-b]==0 && back==0) qq.push({cur-b,1});
+                if(cur+a<7777 && vis[cur+a]==0) qq.push({cur+a,0});      
                 
             }
             ans++;
