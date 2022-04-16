cd033d7ccc4079abb7fa20e9fa399dc4c415c94a
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-16 22:51:55 +0530

    Time: 20 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long long ans=0;
-        
-       // if(cost1>cost2) swap(cost1,cost2);
-        
+    
         for(int i=0;i<=(total/cost1);i++){ 
             long long v1 = i*cost1;
             long long rem = total - v1;
