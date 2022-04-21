77a5553a8a903c471da79b33b8cc16bb7b6d9328
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-22 00:01:24 +0530

    Time: 83 ms (94.90%), Space: 41 MB (77.89%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -2,27 +2,27 @@ class MyHashSet {
 public:
     vector<vector<int>> ar;
     MyHashSet() {
-        ar.resize(1002);
+        ar.resize(997);
     }
     
     void add(int key) {
         if(contains(key)) return;
         
-        ar[key%1000].push_back(key);
+        ar[key%997].push_back(key);
     }
     
     void remove(int key) {
        int j=0;
-        for(int i:ar[key%1000]){
+        for(int i:ar[key%997]){
             if(i==key){  
-               ar[key%1000].erase(ar[key%1000].begin()+j);
+               ar[key%997].erase(ar[key%997].begin()+j);
             }
             j++;
         }
     }
     
     bool contains(int key) {
-        for(int i:ar[key%1000]){
+        for(int i:ar[key%997]){
             if(i==key){  
                 return 1;
             }
