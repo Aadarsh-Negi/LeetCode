514807a4c8b4dcfbf732f7b208a0da5761f7e00d
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-27 19:08:16 +0530

    Time: 775 ms (5.14%), Space: 95.4 MB (5.05%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -8,13 +8,12 @@ public:
             ar[it[1]].push_back(it[0]);
         }
        unordered_map<int,int> vis;
-        // string ans;
-        set<int> ind;
-        multiset<char> cc;
+        vector<int> ind;
+        vector<char> cc;
         function<void(int)> dfs = [&](int i){
             vis[i]=1;
-            ind.insert(i);
-            cc.insert(s[i]);
+            ind.push_back(i);
+            cc.push_back(s[i]);
             
             for(int j:ar[i]){
                 if(!vis[j]){
@@ -27,8 +26,10 @@ public:
         for(int i=0;i<s.size();i++){
             if(!vis[i]) {
                 dfs(i);
-                auto it = cc.begin();
-                for(int j:ind) {s[j] = *it; it++;}
+                sort(ind.begin(),ind.end());
+                sort(cc.begin(),cc.end());
+                int k=0;
+                for(int &j:ind) s[j] = cc[k++];
                 cc.clear();
                 ind.clear();
                 
