ccc25939512bb5c69996332f4796ace89c643ace
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-28 22:21:21 +0530

    Time: 145 ms (93.06%), Space: 40.9 MB (40.30%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool ok(int &x,int i,int j,vector<vector<int>> &vis,vector<vector<int>> &ht){
+        vis[i][j]=1;
+        if(i==ht.size()-1 && ht[0].size()-1 == j) return 1;
+        if(i+1<ht.size()  && !vis[i+1][j] && abs(ht[i][j] - ht[i+1][j])<=x){
+            if(ok(x,i+1,j,vis,ht)) return 1;
+        }
+        if(j+1<ht[0].size()  && !vis[i][j+1] && abs(ht[i][j] - ht[i][j+1])<=x){
+            if(ok(x,i,j+1,vis,ht)) return 1;
+        }
+        if(i-1>=0 && !vis[i-1][j] && abs(ht[i][j] - ht[i-1][j])<=x){
+            if(ok(x,i-1,j,vis,ht)) return 1;
+        }
+        if(j-1>=0 && !vis[i][j-1] &&  abs(ht[i][j] - ht[i][j-1])<=x){
+            if(ok(x,i,j-1,vis,ht)) return 1;
+        }
+        
+        return 0;
+        
+    }
+    
+    int minimumEffortPath(vector<vector<int>>& ht) {
+        int ans=0;
+        int l = 0;
+        int r = 1e6+3;  
+        while(l<=r){
+            
+            int mid = l + (r-l)/2;
+            vector<vector<int>> vis(ht.size(),vector<int>(ht[0].size(),0));
+            if(ok(mid,0,0,vis,ht)){
+                ans=mid;
+                r = mid-1;
+            }else l = mid+1;    
+        }
+        return ans;
+        
+    }
+};
