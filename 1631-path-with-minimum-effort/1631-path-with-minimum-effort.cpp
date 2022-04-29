01ddf51b0f02df4f482866c87d92c38439ba9c6b
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-29 19:28:08 +0530

    Time: 111 ms (99.18%), Space: 19.1 MB (89.29%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,37 +1,41 @@
-int dir_x[] = {1,-1,0,0};
-int dir_y[] = {0,0,1,-1};
 class Solution {
 public:
-    bool ok(int &ans,int i,int j,vector<vector<int>> &vis,vector<vector<int>> &ht){
-        vis[i][j]=1;
-        if(i==ht.size()-1 && ht[0].size()-1 == j) return 1;
-        
-        for(int k=0;k<4;k++){
-            int x = dir_x[k] + i;
-            int y = dir_y[k] + j;
-            if(y<0 || x<0 || x>=ht.size() || y>=ht[0].size() || vis[x][y] || abs(ht[i][j] - ht[x][y])>ans) continue;
-            
-            if(ok(ans,x,y,vis,ht)) return 1;
-            
-        }
-        
-        return 0;
-        
-    }
-    
     int minimumEffortPath(vector<vector<int>>& ht) {
+        priority_queue<pair<int,pair<int,int>>> qq;
         
-        int l = 0;
-        int r = 1e6+3;  
-        while(l<r){
+        qq.push({0,{0,0}});
+        // int ans=1e8;
+        
+        int dir_x[] = {0,0,-1,1};
+        int dir_y[] = {-1,1,0,0};
+        
+        vector<vector<int>> ans(ht.size(),vector<int>(ht[0].size(),INT_MAX));
+        ans[0][0]=0;
+        while(qq.size()){
             
-            int mid = l + (r-l)/2;
-            vector<vector<int>> vis(ht.size(),vector<int>(ht[0].size(),0));
-            if(ok(mid,0,0,vis,ht)){
-                r = mid;
-            }else l = mid+1;    
+            int i = qq.top().second.first;
+            int j = qq.top().second.second;
+            int xx = -qq.top().first;
+            if(i==ht.size()-1  && j==ht[0].size()-1) return xx;
+            qq.pop();
+            for(int k=0;k<4;k++){
+                int x = i+dir_x[k];
+                int y = j+dir_y[k];
+                
+                if(x<0 || y<0 || x>=ht.size() || y>=ht[0].size()) continue;
+                
+                int mx = max(xx,abs(ht[i][j] - ht[x][y]));
+                
+                if(ans[x][y]>mx){
+                    ans[x][y] = mx;
+                    qq.push({-mx,{x,y}});
+                }
+                
+            }
+            
         }
-        return r;
+        return ans[ht.size()-1][ht[0].size()-1];
+        
         
     }
 };
