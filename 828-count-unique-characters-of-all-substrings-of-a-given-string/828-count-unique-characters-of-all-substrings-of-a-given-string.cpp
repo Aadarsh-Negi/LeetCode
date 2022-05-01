cbfe804bd8bff87ca3a94175cbb2d91f7cba9dbe
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-01 13:57:41 +0530

    Time: 153 ms (16.36%), Space: 25 MB (13.32%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -6,17 +6,14 @@ public:
         int n = s.size();
         for(int i=0;i<s.size();i++){
             char c = s[i];
-            if(ind[c].size()==0){
-                ind[c].push_back(0);
-                // ind[c].push_back(n+1);
-            }
+            
+            if(ind[c].size()==0) ind[c].push_back(0);
+            
             ind[c].push_back(i+1);
         }
         int ans=0;
         for(auto &it:ind){
-            // sort(it.second.begin(),it.second.end());
             it.second.push_back(n+1);
-            
             for(int i=1;i<it.second.size()-1;i++){
                 int prev = it.second[i] - it.second[i-1];
                 prev *= it.second[i+1] - it.second[i];
