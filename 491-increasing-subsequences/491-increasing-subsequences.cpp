d3c8f86df3cbdf7037a39ceb6dd0212ba1add74c
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-18 01:17:04 +0530

    Time: 967 ms (5.11%), Space: 295.5 MB (5.11%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.cpp
+++ 491-increasing-subsequences/491-increasing-subsequences.cpp
@@ -1,18 +1,62 @@
+class Trie {
+public:
+    int end;
+    Trie *ar[300];
+    Trie() {
+        end=0;
+        for(int i=0;i<300;i++) this->ar[i]=NULL;
+    }
+    
+    void insert(vector<int> &word) {
+        Trie *cur = this;
+        int cnt=1;
+        for(int &c:word){
+            c = c+100;
+            if(!cur->ar[c]) cur->ar[c] = new Trie;
+            cur = cur->ar[c];
+            if(cnt==word.size()) cur->end=1;
+            cnt++;
+        }
+    }
+    
+    bool search(vector<int>& word) {
+        Trie * cur =this;
+        int cnt=1;
+        for(int c:word){
+            c = c+100;
+            if(cur->ar[c])
+                cur = cur->ar[c];
+            else return 0;
+            if(cnt==word.size() && cur->end) {cur->end=0; return 1;}
+            cnt++;
+        }
+        return 0;
+    }
+};
 class Solution {
 public:
    
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         
         vector<vector<int>> ans;
+        Trie *tx = new Trie();
+        for(int i=1;i<(1<<nums.size());i++){
+            vector<int> temp;
+            for(int j=0;j<nums.size();j++)
+                if((1<<j)&i) temp.push_back(nums[j]);           
+            if(temp.size()>1 && is_sorted(temp.begin(),temp.end()))
+                tx->insert(temp);  
+        }
         
         for(int i=1;i<(1<<nums.size());i++){
             vector<int> temp;
             for(int j=0;j<nums.size();j++)
                 if((1<<j)&i) temp.push_back(nums[j]);           
-            if(temp.size()>1 && is_sorted(temp.begin(),temp.end())) ans.push_back(temp);
+            if(tx->search(temp)) ans.push_back(temp);
         }
-        sort(ans.begin(),ans.end());
-        ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        
+        
+        
         return ans;
     }
 };
