2d86dee3ce0dcc7a099a83ac76fb3e639bca5119
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-17 07:57:38 +0530

    Time: 777 ms (37.89%), Space: 163.9 MB (58.30%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -13,10 +13,10 @@ public:
     TreeNode* solve(TreeNode* r2,TreeNode* r1,TreeNode* tar){
         if(!r2) return NULL;
         if(r2==tar) return r1;
-        TreeNode* l = solve(r2->left,r1->left,tar);
-        if(l) return l;
-        l = solve(r2->right,r1->right,tar);
-        return l;
+        // TreeNode* l = 
+        // if(l) return l;
+        return (solve(r2->left,r1->left,tar)==NULL?solve(r2->right,r1->right,tar):solve(r2->left,r1->left,tar));
+        
     }
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target){
         return solve(original,cloned,target);
