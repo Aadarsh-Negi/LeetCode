1b7ab0755df50041c494168e3f6907267f3cef21
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-18 07:13:42 +0530

    Time: 1022 ms (33.64%), Space: 167.4 MB (82.07%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -32,19 +32,19 @@ function<void(int,int)> dfs = [&](int v, int p = -1) {
     }
 };
 
-auto find_bridges = [&]() {
+// auto find_bridges = [&]() {
     timer = 0;
     visited.assign(n, false);
     tin.assign(n, -1);
     low.assign(n, -1);
-    for (int i = 0; i < n; ++i) {
-        if (!visited[i])
-            dfs(i,-1);
-    }
-};
+    // for (int i = 0; i < n; ++i) {
+    //     if (!visited[i])
+            dfs(0,-1);
+//     }
+// };
         
         
-     find_bridges();   
+     // find_bridges();   
         return ans;
     }
 };
