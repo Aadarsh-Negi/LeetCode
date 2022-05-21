db02745fc1ab88777f0c09162831cc8a9a03dabf
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-21 10:39:19 +0530

    Time: 1292 ms (5.02%), Space: 119.7 MB (5.05%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,20 +1,26 @@
 
 class Solution {
 public:
-    int solve(int ss,vector<int> &ar,int *dp){
-        if(ss==0) return 0;
-        if(ss<0) return INT_MAX/2;
-        if(dp[ss]!=-1) return dp[ss];
-        int temp=INT_MAX;
-        for(int j=0;j<ar.size();j++){
-            temp = min(temp,1+solve(ss-ar[j],ar,dp));
-        }
-        return dp[ss]=temp;
-    }
+
     int coinChange(vector<int>& ar, int ss) {
-        int dp[ss+1];
-        memset(dp,-1,sizeof(dp));
-        int ans = solve(ss,ar,dp);
-        return ans>=INT_MAX/2?-1:ans;
+        int ans=0;
+        
+        queue<long long> qq;
+        qq.push(0);
+        map<int,int> vis;
+        while(qq.size()){
+            int x = qq.size();
+            for(int i=0;i<x;i++){
+                long long ax = qq.front(); qq.pop();
+                if(vis[ax]) continue;
+                vis[ax]=1;
+                if(ax==ss) return ans;
+                for(int &i:ar)
+                    if(i+ax<=ss) qq.push(i+ax);
+            }
+            ans++;
+        }
+        return -1;
+        
     }
 };
