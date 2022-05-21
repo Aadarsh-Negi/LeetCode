2ca38f5c361983f9b1f49e54c4eff8f1c656db5c
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-21 13:47:20 +0530

    Time: 168 ms (64.47%), Space: 110.9 MB (14.97%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-    int waysToMakeFair(vector<int>& ar) {
-        int even=0;
-        int odd=0;
-        for(int i=0;i<ar.size();i++){
-            if(i&1) even+=ar[i];
-            else odd+=ar[i];
+    int n;
+    vector<int> ar;
+    vector<int> pref;
+    int solve(int i,int cur_even,int cur_odd,int fl){
+        if(i>n){
+            if(!fl) return 0;
+            return cur_even==cur_odd;
         }
-        
-        int ans=0;
-        int cur_odd=0;
-        int cur_even=0;
-        for(int i=0;i<ar.size();i++){
-            if(i&1){ // even pos
-                // even-=ar[i];
-                int nx_even = cur_even + (odd - cur_odd);
-                int nx_odd = cur_odd + (even - cur_even-ar[i]);
-                if(nx_even == nx_odd) ans++;
-                cur_even+=ar[i];
-            }else{ // odd pos
+        // if(dp.count({i,fl})) r
+        int temp=0;
+        if(fl){ // fl=1 if removed
+           if(i&1){
+               temp+=solve(n+1,cur_even+pref[i],cur_odd+pref[i+1],1);
+           }else{
+               temp+=solve(n+1,cur_even+pref[i+1],cur_odd+pref[i],1);
+           }
+            
+        }else{
+            if(i&1){
+                temp+=solve(i+1,cur_even,cur_odd+ar[i-1],0);
+                temp+=solve(i+1,cur_even,cur_odd,1);  
+            }else{
+                temp+=solve(i+1,cur_even+ar[i-1],cur_odd,0);  
+                temp+=solve(i+1,cur_even,cur_odd,1);
                 
-                // odd-=ar[i];
-                int nx_even = cur_even + (odd - cur_odd-ar[i]);
-                int nx_odd = cur_odd + (even - cur_even);
-                if(nx_even == nx_odd) ans++;
-                cur_odd+=ar[i];
             }
         }
+        return temp;
         
-        return ans;
+    }
+    
+    
+    int waysToMakeFair(vector<int>& ar2) {
+//         ttl_even=0;
+//         ttl_odd=0;
+        ar=ar2;
+        n=ar.size();
         
+        pref.resize(n+3,0);
+        pref[n] = ar2[n-1];
+        if(n>=2)
+            pref[n-1] = ar2[n-2];
+        for(int i=n-2;i>0;i--) pref[i]+=pref[i+2]+ar[i-1];
         
+        return solve(1,0,0,0);
     }
 };
