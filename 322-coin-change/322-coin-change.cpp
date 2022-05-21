0d8a0be502838a417aac1714966a5c42903d5d8e
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-21 08:32:25 +0530

    Time: 65 ms (90.62%), Space: 10.3 MB (89.97%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,20 @@
 
 class Solution {
 public:
-    int solve(int i,int ss,vector<int> &ar,vector<vector<int>> &dp){
+    int solve(int ss,vector<int> &ar,int *dp){
         if(ss==0) return 0;
-        if(i>=ar.size() || ss<0) return INT_MAX/2;
-        if(dp[ss][i]!=-1) return dp[ss][i];
+        if(ss<0) return INT_MAX/2;
+        if(dp[ss]!=-1) return dp[ss];
         int temp=INT_MAX;
-        for(int j=i;j<ar.size();j++){
-            temp = min(temp,1+solve(j,ss-ar[j],ar,dp));
-            temp = min(temp,solve(j+1,ss,ar,dp));
+        for(int j=0;j<ar.size();j++){
+            temp = min(temp,1+solve(ss-ar[j],ar,dp));
         }
-        return dp[ss][i]=temp;
+        return dp[ss]=temp;
     }
     int coinChange(vector<int>& ar, int ss) {
-        vector<vector<int>> dp(ss+1,vector<int>(ar.size()+1,-1));
-        // memset(dp,-1,sizeof(dp));
-        int ans = solve(0,ss,ar,dp);
+        int dp[ss+1];
+        memset(dp,-1,sizeof(dp));
+        int ans = solve(ss,ar,dp);
         return ans>=INT_MAX/2?-1:ans;
     }
 };
