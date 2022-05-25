6cc17b63f449f472ea9b4d22a9d3998e98ed9835
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-25 13:43:01 +0530

    Time: 550 ms (16.15%), Space: 10.5 MB (67.17%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,13 +1,33 @@
+int dp[2600];
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> ar;
+    
+    int solve(int i,vector<int> &ar){
+        if(i==ar.size()) return 0;
         
-        for(int i:nums){
-            int it = lower_bound(ar.begin(),ar.end(),i)-ar.begin();
-            if(it==ar.size()) ar.push_back(i);
-            else ar[it] = i;
+        if(i==-1){
+            int tx = -1;
+            for(int k=0;k<ar.size();k++){
+                tx = max(tx,solve(k,ar));
+            }
+            return tx;
         }
-        return ar.size();
+        if(dp[i]!=-1) return dp[i];
+        int temp = -1;
+        for(int j=i+1;j<ar.size();j++){
+            if(ar[j]>ar[i]){
+                temp = max(temp,1+solve(j,ar));
+            }
+        }
+        
+        
+        return dp[i] = temp;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        // ans=-1;
+        return solve(-1,nums)+2;
+        // return ans;
     }
 };
