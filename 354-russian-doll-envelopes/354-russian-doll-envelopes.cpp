4a222ef5b9a93b9af1af975b93e994fecd723e4a
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-25 08:13:59 +0530

    Time: 557 ms (34.30%), Space: 102.7 MB (5.05%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    int maxEnvelopes(vector<vector<int>>& env) {
+        sort(env.begin(),env.end(),[&](vector<int> &a,vector<int> &b){
+            if(a[0]==b[0]) return a[1]>b[1];
+            return a[0]<b[0];
+        });
+        // sort(env.begin(),env.end());
+        
+        set<int> S;
+       for (auto x : env){
+           if(S.count(x[1])) continue;
+            S.insert(x[1]);
+            auto it = S.upper_bound(x[1]);
+            if(it != S.end()) S.erase(it);
+        }
+        return S.size();
+        
+    }
+};
