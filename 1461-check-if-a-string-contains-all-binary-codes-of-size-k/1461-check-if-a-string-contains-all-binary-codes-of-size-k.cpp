3cf1a939fbd1b379660db414b5886980cd6d1619
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-05-31 07:50:29 +0530

    Time: 555 ms (38.99%), Space: 44.3 MB (66.04%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
+    long long compute_hash(string const& s) {
+    const int p = 31;
+    const int m = 1e9 + 9;
+    long long hash_value = 0;
+    long long p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
     bool hasAllCodes(string s, int k) {
-        set<string> cnt;
+        set<int> cnt;
         
         string temp;
         for(char &c:s){
             temp+=c;
             if(temp.size()>=k){
-                cnt.insert(temp);
+                cnt.insert(compute_hash(temp));
                 temp.erase(temp.begin());
             }
         }
-        // for(string sx:cnt) cout<<sx<<" ";
-        // k--;
+      
         return cnt.size() == (1<<k);
         
     }
