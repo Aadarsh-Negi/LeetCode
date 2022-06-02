fbf3994ccf45a05661afa51806632d4c1e6985b5
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-02 10:17:58 +0530

    Time: 101 ms (5.00%), Space: 5.8 MB (83.70%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int ans;
-    
-    void solve(int n,int fl=-1){
-        if(n==0) {ans++; return;}
-        
+    int solve(int n,int fl=-1){
+        if(n==0) {return 1;}
+        int temp=0;
         for(int i=(fl==-1?1:0);i<=9;i++){
             if(vis[i]) continue;
             vis[i]=1;
-            solve(n-1,1);
+            temp+=solve(n-1,1);
             vis[i]=0;
         }
+        return temp;
     }
     
     vector<int> vis;
     int countNumbersWithUniqueDigits(int n) {
-        ans=0;
+        int ans=0;
         vis.resize(10,0);
-            while(n!=-1) solve(n),n--;
+            while(n!=-1) ans+=solve(n),n--;
         return ans;
   
     }
