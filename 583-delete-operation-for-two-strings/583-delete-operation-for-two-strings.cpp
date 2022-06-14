628b4fd40c2a9fb856619cdf53476d2c4eadd47b
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-14 06:21:58 +0530

    Time: 72 ms (15.32%), Space: 12.7 MB (9.26%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -17,7 +17,7 @@ public:
         for(int i=1;i<=s1.size();i++){
             for(int j=1;j<=s2.size();j++){
                 if(s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                 dp[i][j] = max({dp[i][j],dp[i-1][j],dp[i][j-1]});
             }
         }
         
