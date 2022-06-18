f0c5e719c49fa42ea4b9074c6118d22c30bef0b7
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-18 12:15:14 +0530

    Time: 391 ms (94.40%), Space: 180.6 MB (50.37%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,9 +1,10 @@
 class TrieNode {
 public:
-    map<int,TrieNode*> children;
+    TrieNode *children[27];
     int weight;
      TrieNode() {
-        // children = new HashMap();
+        // children = new TrieNode[27];
+        for(int i=0;i<27;i++) children[i] = NULL;
         weight = 0;
     }
 };
@@ -12,54 +13,28 @@ public:
     TrieNode *trie;
      WordFilter(vector<string> &words) {
         trie = new TrieNode();
-        int wt = 0;
-        for (string &word: words) {
-            TrieNode *cur = trie;
-            cur->weight = wt;
-            int L = word.size();
-            // char[] chars = word.toCharArray();
-            for (int i = 0; i < L; ++i) {
-
-                TrieNode *tmp = cur;
-                for (int j = i; j < L; ++j) {
-                    int code = (word[j] - '`') * 27;
-                    if (tmp->children.count(code) == 0)
-                        tmp->children[code] =  new TrieNode();
-                    tmp = tmp->children[code];
-                    tmp->weight = wt;
+        for (int weight = 0; weight < words.size(); ++weight) {
+            string word = words[weight] + "{";
+            for (int i = 0; i < word.length(); ++i) {
+                TrieNode *cur = trie;
+                cur->weight = weight;
+                for (int j = i; j < 2 * word.length() - 1; ++j) {
+                    int k = word[j % word.length()] - 'a';
+                    if (cur->children[k] == NULL)
+                        cur->children[k] = new TrieNode();
+                    cur = cur->children[k];
+                    cur->weight = weight;
                 }
-
-                tmp = cur;
-                for (int k = L - 1 - i; k >= 0; --k) {
-                    int code = (word[k] - '`');
-                    if (tmp->children.count(code) == 0)
-                        tmp->children[code] = new TrieNode();
-                    tmp = tmp->children[code];
-                    tmp->weight = wt;
-                }
-
-                int code = (word[i] - '`') * 27 + (word[L - 1 - i] - '`');
-                if (cur->children.count(code) == 0)
-                    cur->children[code] = new TrieNode();
-                cur = cur->children[code];
-                cur->weight = wt;
-
             }
-            wt++;
         }
     }
-
      int f(string prefix, string suffix) {
         TrieNode *cur = trie;
-        int i = 0, j = suffix.length() - 1;
-        while (i < prefix.length() || j >= 0) {
-            char c1 = i < prefix.length() ? prefix[i] : '`';
-            char c2 = j >= 0 ? suffix[j] : '`';
-            int code = (c1 - '`') * 27 + (c2 - '`');
-            // cur = cur->children[.get(code);]
-            if (cur->children.count(code) == 0) return -1;
-            cur = cur->children[code];
-            i++; j--;
+        suffix+='{';
+        suffix+=prefix;
+        for (char &letter:suffix) {
+            if (cur->children[letter - 'a'] == NULL) return -1;
+            cur = cur->children[letter - 'a'];
         }
         return cur->weight;
     }
