468350f7f9696da9f10f89fb2b7e6525c9bd170b
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-22 12:55:35 +0530

    Time: 177 ms (38.56%), Space: 76.2 MB (5.44%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        
-    sort(words.begin(), words.end(), [](const string& first, const string& second)
-	{
-        return first.size() < second.size();
-	});
-        
-        map<string,int> m;
-        int res = 0;
-        
-        for(string word:words)
-        {
-            int longest =0;
-            for(int i = 0;i<word.length();i++)
-            {
-                string sub = word.substr(0,i) + word.substr(i+1,word.length()+1);
-                longest = max(longest,m[sub]+1);   
-            }
-            
-            m[word] = longest;
-            res = max(res,longest);
+        unordered_map<string,int> avl;
+        for(string &s:words){
+            avl[s]=1;
         }
-        return res;
+        
+        sort(words.begin(),words.end(),[&](const string &a,const string &b){
+           return a.size() > b.size();
+        });
+        unordered_map<string,int> done;
+        
+        int ans=0;
+        for(string &s:words){
+            if(done[s]) continue;
+            // done[s]=1;
+            queue<string> qq;
+            qq.push(s);
+            int cnt=0;
+            while(qq.size()){
+                int x = qq.size();
+                
+                while(x--){
+                    
+                    string cur = qq.front(); qq.pop();
+                    if(done[cur]) continue;
+                    done[cur]=1;
+                    int sx = cur.size();
+                    for(int i=0;i<sx;i++){
+                        string temp = cur;
+                        temp.erase(temp.begin()+i);
+                        if(avl[temp]){
+                            qq.push(temp);
+                        }
+                    }
+                }
+                cnt++;
+            }
+            ans = max(ans,cnt);
+            
+            
+        }
+        return ans;
+        
+        
     }
 };
