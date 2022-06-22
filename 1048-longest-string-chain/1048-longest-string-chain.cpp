6221b649d4a0557d5a6c84a45a30f949265bc9c1
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-22 12:58:49 +0530

    Time: 283 ms (19.12%), Space: 60.8 MB (6.27%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        unordered_map<string,int> avl;
-        for(string &s:words){
-            avl[s]=1;
-        }
         
         sort(words.begin(),words.end(),[&](const string &a,const string &b){
-           return a.size() > b.size();
+           return a.size() < b.size();
         });
-        unordered_map<string,int> done;
         
+        unordered_map<string,int> prev;
         int ans=0;
         for(string &s:words){
-            if(done[s]) continue;
-            // done[s]=1;
-            queue<string> qq;
-            qq.push(s);
-            int cnt=0;
-            while(qq.size()){
-                int x = qq.size();
-                
-                while(x--){
-                    
-                    string cur = qq.front(); qq.pop();
-                    if(done[cur]) continue;
-                    done[cur]=1;
-                    int sx = cur.size();
-                    for(int i=0;i<sx;i++){
-                        string temp = cur;
-                        temp.erase(temp.begin()+i);
-                        if(avl[temp]){
-                            qq.push(temp);
-                        }
-                    }
-                }
-                cnt++;
+            int len=0;
+            for(int i=0;i<s.size();i++){
+                string temp = s;
+                temp.erase(temp.begin()+i);
+                len=max(len,prev[temp]+1);
             }
-            ans = max(ans,cnt);
-            
-            
+            prev[s] = len;
+            ans=max(len,ans);
         }
         return ans;
         
