3c61892834ea63f23eae25c1702736d86d43a189
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-22 10:09:48 +0530

    Time: 260 ms (5.02%), Space: 10 MB (61.91%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int qk(vector<int> &ar,int l,int h,int k){
-        if(l<=h){
-            int mid = part(ar,l,h);
-            if(mid==ar.size()-k) return ar[mid];
-             if(mid<ar.size()-k){
-                return qk(ar,mid+1,h,k);
-            }
-            return qk(ar,l,mid-1,k);
-        }
-        return -1;
-    }
-    int part(vector<int> &ar,int l,int h){
-        int last = l-1;
-        int pt = ar[h];
-        for(int j=l;j<h;j++){
-            if(ar[j]<pt){
-                last++;
-                swap(ar[j],ar[last]);
-            }
-        }
-        swap(ar[last+1],ar[h]);
-        return last+1;
+    int qs(vector<int> &nums,int l,int h,int k){
+        if(l>h) return -1;
+        
+        int mid = split(nums,l,h);
+        
+        if(mid == nums.size() - k) return nums[mid];
+        
+        if(mid > nums.size()-k) return qs(nums,l,mid-1,k);
+        return qs(nums,mid+1,h,k);
+        
         
     }
+    
+    int split(vector<int> &ar,int l,int h){
+        int last = l;
+        while(l<h){
+            if(ar[l]<ar[h]){
+                swap(ar[l],ar[last++]);
+            }
+            l++;
+        }
+        swap(ar[h],ar[last]);
+        return last;
+    }
+    
     int findKthLargest(vector<int>& nums, int k) {
-        return qk(nums,0,nums.size()-1,k);
+        return qs(nums,0,nums.size()-1,k);
     }
 };
