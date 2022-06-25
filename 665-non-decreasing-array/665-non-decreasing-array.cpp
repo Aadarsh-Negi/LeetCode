584e53cad739d4337b5ae6608570e4f9712683f2
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-25 14:09:57 +0530

    Time: 38 ms (60.32%), Space: 27 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         nums.push_back(1e6);
-        for(int i=0;i<nums.size()-1;i++){
+        for(int i=0;i<nums.size()-2;i++){
             if(nums[i] > nums[i+1]){
                 int prev = nums[i];
                 nums[i]=nums[i+1];
                 if(is_sorted(nums.begin(),nums.end())) return 1;
                 
                 nums[i]=prev;
-                if(i+2<nums.size()){
+                // if(i+2<nums.size()){
                     nums[i+1] = nums[i+2];
                     if(is_sorted(nums.begin(),nums.end())) return 1;
-                }
+                // }
                 
                 return 0;
                 
