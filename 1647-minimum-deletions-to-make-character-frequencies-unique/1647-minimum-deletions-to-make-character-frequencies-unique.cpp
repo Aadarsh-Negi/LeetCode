8b3d111b3e6f31134ae14d79ab5a1901414fc989
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-06-28 09:47:37 +0530

    Time: 99 ms (45.90%), Space: 17.3 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        set<int> ix;
-        for(int i=0;i<=s.size();i++) ix.insert(i);
+        // set<int> ix;
+        // for(int i=0;i<=s.size();i++) ix.insert(i);
         vector<int> freq(26,0);
         for(char &c:s){
             freq[c-'a']++;
         }
         int ans=0;
-        for(int i:freq){
-            if(i){
-                auto it = ix.upper_bound(i); it--;
-                
-                ans+=abs(i-*it);
-                if(*it != 0) ix.erase(it);
-                
+        sort(freq.rbegin(),freq.rend());
+        int prev=-1;
+        for(int &i:freq){
+            if(!i) break;
+            if(prev==-1) prev = i;
+            else{
+                if(prev<=i){
+                    ans+= i - max(0,(prev-1));
+                    i = max(0,prev-1);
+                    // i =
+                }
             }
+            prev=i;
+            // cout<<prev<<" ";
         }
+//         for(int i:freq){
+//             if(i){
+//                 auto it = ix.upper_bound(i); it--;
+                
+//                 ans+=abs(i-*it);
+//                 ix.erase(it);
+                
+//             }
+//         }
         return ans;
         
     }
