ead6a077304d284a448e0d8ce076900aa2271d28
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-03 11:36:58 +0530

    Time: 497 ms (50.00%), Space: 48 MB (50.00%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,40 +1,8 @@
-#define ll long long
-#define pb push_back
-#define ppb pop_back
-#define ins insert
-#define ff first
-#define lb lower_bound
-#define ub upper_bound
-#define ss second
-#define mset multiset
-#define vi vector<int>
-#define vs vector<string>
-#define vvi vector<vi>
-#define umap unordered_map
-#define all(xxx90) xxx90.begin(),xxx90.end()
-#define allr(xxx90) xxx90.rbegin(),xxx90.rend()
-template<class T> using mxpq = priority_queue<T>;
-template<class T> using mnpq = priority_queue<T, vector<T>, greater<T>>;
-int getmx(vector<int> &ar){return *max_element(all(ar));}
-int getmn(vector<int> &ar){return *min_element(all(ar));}
-int mnind(vector<int> &ar){return min_element(all(ar))-ar.begin();}
-int mxind(vector<int> &ar){return max_element(all(ar))-ar.begin();}
-int gcd(int a,int b){ return __gcd(a,b);}
-int lcm(int a,int b){ return ((a/__gcd(a,b))*b);}
-ll add_mod(ll a,ll b,ll m){return ((((a%m)+(b%m))%m + m)%m);}
-ll subs_mod(ll a,ll b,ll m){return ((((a%m)-(b%m))%m + m)%m);}
-ll mult_mod(ll a,ll b,ll m){return ((((a%m)*(b%m))%m + m)%m);}
-ll power(ll x, ll y){ ll res = 1ll; while (y > 0ll) { if (y & 1ll)res = res * x; y = y >> 1ll; x = x * x;}return res;}
-ll power_mod(ll x, ll y,ll mod){ ll res = 1ll; while (y > 0ll) { if (y & 1ll) res = mult_mod(res, x,mod); y = y >> 1ll; x = mult_mod(x,x,mod);}return res;}
-const ll mod = 1000000007;
-const ll inf = 1e18; 
-const ll mod2 = 998244353;
-
 class Solution {
 public:
     int n,m;
-    vvi cnt;
-    vvi g;
+    vector<vector<int>> cnt;
+    vector<vector<int>> g;
     int const mod = 1e9 + 7;
     int dx[4] = {1,-1,0,0};
     int dy[4] = {0,0,-1,1};
@@ -57,7 +25,7 @@ public:
         g=g2;
         n = g.size();
         m = g[0].size();
-        cnt.resize(n,vi(m,-1));
+        cnt.resize(n,vector<int>(m,-1));
         
         long long ans=n*m;
         for(int i=0;i<n;i++) {
