22c54ca28151daa766c0d767a8fa0a9125bc7840
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-04 08:59:07 +0530

    Time: 34 ms (49.44%), Space: 19.6 MB (9.20%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        vector<int> ok(r.size(),-1);
+        
+        int mn = min_element(r.begin(),r.end()) - r.begin();
+        
+        ok[mn]=1;
+        int m2 = mn+1;
+        int m1 = mn-1;
+        while(m2<r.size() || m1>=0){
+            while(m2<r.size() && r[m2] > r[m2-1]) ok[m2] = ok[m2-1]+1,m2++;
+            while(m1>=0 && r[m1] > r[m1+1]) ok[m1] = ok[m1+1]+1,m1--;
+            
+            if(m2<r.size()) ok[m2++] = 1;
+            if(m1>=0) ok[m1--] = 1;
+            // for(int i:ok) cout<<i<<" ";cout<<"\n";
+        }
+        // cout<<"\n";
+        
+    while(1){
+        vector<int> temp = ok;
+            for(int i=1;i<r.size()-1;i++){
+            if(r[i]>r[i+1] && ok[i+1]>=ok[i]){
+                ok[i] = ok[i+1]+1;
+            }else if(r[i]>r[i-1] && ok[i-1]>=ok[i]){
+                ok[i] = ok[i-1]+1;
+            }
+        }
+        if(ok==temp) break;
+    }
+        // for(int i:ok) cout<<i<<" ";cout<<"\n";
+        
+        
+
+        
+        
+        return accumulate(ok.begin(),ok.end(),0);
+        
+        
+    }
+};
