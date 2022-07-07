46141880a1faaa041c47f81fb89c69aa7cd19d2e
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-07 11:04:46 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,14 +1,14 @@
-int dp[103][103][203];
+int dp[103][103];
 class Solution {
 public:
     bool solve(string &a,string &b,string &c,int i=0,int j=0,int k=0){
         if(i==a.size() && j==b.size() && k==c.size()) return 1;
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
-        
+        if(dp[i][j]!=-1) return dp[i][j];
+        // if(k==c.size()) return 0;
         int temp = 0;
         if(i<a.size() && a[i] == c[k]) temp|=solve(a,b,c,i+1,j,k+1);
         if(j<b.size() && b[j] == c[k]) temp|=solve(a,b,c,i,j+1,k+1);
-        return dp[i][j][k] = temp;
+        return dp[i][j] = temp;
     }
     bool isInterleave(string &s1, string &s2, string &s3) {
        memset(dp,-1,sizeof(dp));
