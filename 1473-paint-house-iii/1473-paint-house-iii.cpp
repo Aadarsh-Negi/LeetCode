acc54bacfa23b1ac7986dbbe65459e8847b96321
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-08 07:55:03 +0530

    Time: 437 ms (5.32%), Space: 10.8 MB (77.13%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -4,23 +4,16 @@ public:
     int minCost(vector<int>& h, vector<vector<int>>& cost, int m, int n, int t) {
         memset(dp,-1,sizeof(dp));
         function<long long(int,int,int)> solve = [&](int tar,int i,int prev){
-            if(i==h.size() && tar==1) return (long long)0;
-            if(i>=h.size() || tar<=0) return (long long)INT_MAX;
+            if(i==h.size() && tar==0) return (long long)0;
+            if(i>=h.size() || tar<0) return (long long)INT_MAX;
+            
             if(dp[i][tar][prev]!=-1) return (long long)dp[i][tar][prev];
             long long temp=INT_MAX;
-            if(h[i]!=0){
-                if(i-1>=0)
-                    temp = solve(tar - (h[i] != prev),i+1,h[i]);
-                else temp = solve(tar,i+1,h[i]);
-            }else{
+      
                 for(int j=1;j<=n;j++){
-                    if(i-1>=0)
-                        temp = min(temp,cost[i][j-1] + solve(tar - (j != prev),i+1,j));
-                    else temp = min(temp,cost[i][j-1] + solve(tar,i+1,j));
-                    
+                    temp = min(temp,(h[i] ? 0 : cost[i][j-1]) + solve(tar - ((h[i] ? h[i] : j) != prev),i+1,h[i] ? h[i] : j));
                 }
-            }
-            
+     
             dp[i][tar][prev] = temp;
             return temp;
         };
