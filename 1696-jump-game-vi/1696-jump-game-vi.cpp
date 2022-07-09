659b209684e2190099cb969b204febe15cf40614
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-09 11:47:00 +0530

    Time: 316 ms (48.62%), Space: 78.1 MB (88.27%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>> ar;
-        for(int i=nums.size()-1;i>=0;i--){
-            while(ar.size() && ar.top().second > i+k) ar.pop();
-            int s = nums[i] + (ar.size() ? ar.top().first : 0);
-            ar.push({s,i});
+        deque<pair<int,int>> ar;
+        int n = nums.size();
+        
+        for(int i=0;i<n;i++){
+            
+            if(ar.size() && ar.front().second < i-k) ar.pop_front();
+            
+            int s = (ar.size() ? ar.front().first : 0) + nums[i];
+            
+            while(ar.size() && ar.back().first <= s) ar.pop_back();
+            
+            ar.push_back({s,i});
         } 
-        while(ar.top().second!=0) ar.pop();
-        
-        return ar.top().first;
-        
+        return ar.back().first;
     }
 };
