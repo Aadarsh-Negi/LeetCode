9e52d8dcd0c0eb35ee4f67ec421506bc0cdde257
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-17 11:54:44 +0530

    Time: 1776 ms (11.11%), Space: 372.3 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
-    vector<int> smallestTrimmedNumbers(vector<string>& ar, vector<vector<int>>& q) {
-        
-        vector<pair<string,int>> part;
-        vector<int> ans;
-        for(int i=0;i<ar.size();i++){
-            // string s = ar[i];
-            // string temp;
-            // int c=1;
-            // for(int j=s.size()-1;j>=0;j--){
-            //     temp = s[j] + temp;
-                part.push_back({ar[i],i});
-            //     c++;
-            // }
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> res;
+        int n = nums.size();
+        for(auto &q:queries) {
+            vector<pair<string,int>> pq;
+            for(int i=0;i<n; i++) {
+                pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
+            }
+            sort(pq.begin(), pq.end());
+            res.push_back(pq[q[0]-1].second);
         }
-        
-        
-        // for(auto &it:part){
-        //     // for(auto ix:it){
-        //     //     cout<<ix.first<<" "<<ix.second<<"\n";
-        //     // }
-        //     // cout<<"\n";
-        //     sort(it.begin(),it.end());
-        // }
-        int n = ar[0].size();
-        
-        for(auto it:q){
-            int c = it[1];
-            int k = it[0];
-            // cout<<c<<" "<<k<<" "<<part[c].size()<<" >";
-            // for(pair<string,int> i:part[c]) cout<<i.first<<" "<<i.second<<" >"<<" ";cout<<"\n";
-            nth_element(begin(part), begin(part) + k - 1, end(part), [&](const auto &a, const auto &b){
-            int cmp = a.first.compare(n - c, string::npos, b.first, n - c, string::npos);
-            return cmp == 0 ? a.second < b.second : cmp < 0;
-        });
-            ans.push_back((part[k-1].second));
-        }
-        return ans;
-        
+        return res;
     }
 };
