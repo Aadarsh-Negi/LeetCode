50364d2dd80398ecb9ffab8c180e7d01cce5f30a
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-17 11:51:08 +0530

    Time: 490 ms (55.56%), Space: 34.6 MB (77.78%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
-vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-    int n = nums[0].size();
-    vector<pair<string, int>> pairs;
-    for (int i = 0; i < nums.size(); ++i)
-        pairs.push_back({ nums[i], i });
-    vector<int> res;
-    for(auto &q : queries) {
-        int k = q[0], trim = q[1];
-        nth_element(begin(pairs), begin(pairs) + k - 1, end(pairs), [&](const auto &a, const auto &b){
-            int cmp = a.first.compare(n - trim, string::npos, b.first, n - trim, string::npos);
+    vector<int> smallestTrimmedNumbers(vector<string>& ar, vector<vector<int>>& q) {
+        
+        vector<pair<string,int>> part;
+        vector<int> ans;
+        for(int i=0;i<ar.size();i++){
+            // string s = ar[i];
+            // string temp;
+            // int c=1;
+            // for(int j=s.size()-1;j>=0;j--){
+            //     temp = s[j] + temp;
+                part.push_back({ar[i],i});
+            //     c++;
+            // }
+        }
+        
+        
+        // for(auto &it:part){
+        //     // for(auto ix:it){
+        //     //     cout<<ix.first<<" "<<ix.second<<"\n";
+        //     // }
+        //     // cout<<"\n";
+        //     sort(it.begin(),it.end());
+        // }
+        int n = ar[0].size();
+        
+        for(auto it:q){
+            int c = it[1];
+            int k = it[0];
+            // cout<<c<<" "<<k<<" "<<part[c].size()<<" >";
+            // for(pair<string,int> i:part[c]) cout<<i.first<<" "<<i.second<<" >"<<" ";cout<<"\n";
+            nth_element(begin(part), begin(part) + k - 1, end(part), [&](const auto &a, const auto &b){
+            int cmp = a.first.compare(n - c, string::npos, b.first, n - c, string::npos);
             return cmp == 0 ? a.second < b.second : cmp < 0;
         });
-        res.push_back(pairs[k - 1].second);
+            ans.push_back((part[k-1].second));
+        }
+        return ans;
+        
     }
-    return res;
-}
 };
