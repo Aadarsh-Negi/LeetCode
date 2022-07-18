1a515d3b60998cfba503ef90fec114062dbb4e4f
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-07-18 13:39:14 +0530

    Time: 586 ms (98.69%), Space: 9.1 MB (92.29%) - LeetHub

---
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& mat, int tar) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int pref[n+1][m+1];
+        
+        memset(pref,0,sizeof(pref));
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                pref[i][j] = pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1] + mat[i-1][j-1];
+            }
+        }
+        
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                for(int i2=i;i2<=n;i2++){
+                    for(int j2=j;j2<=m;j2++){
+                        int val = pref[i2][j2] - pref[i2][j-1] - pref[i-1][j2] + pref[i-1][j-1];
+                        if(val==tar) ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+        
+        
+            
+    }
+};
