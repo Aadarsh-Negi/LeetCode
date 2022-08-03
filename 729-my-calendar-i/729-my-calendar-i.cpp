1d9efe97395b4c73d301916811018012c69b3b97
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-03 15:41:03 +0530

    Time: 224 ms (28.17%), Space: 37.7 MB (87.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -3,16 +3,16 @@ public:
     MyCalendar() {
         
     }
+    bool cross(pair<int, int>& a, pair<int, int> b){
+        return max(a.first, b.first) <= min(a.second, b.second);
+    }
     vector<pair<int,int>> ar;
     
     bool book(int start, int end) {
         end--;
-        for(auto &it : ar){
-            if(it.first<=start && it.second>=start) return 0;
-            if(it.first<=end and it.second>=end) return 0;
-            if(it.first<=end and it.first>=start) return 0;
-            if(it.second>=start and it.second<=end) return 0;
-        }
+        for(auto &it : ar)
+            if(cross(it,{start,end})) return 0;
+           
         ar.push_back({start,end});
         return 1;
     }
