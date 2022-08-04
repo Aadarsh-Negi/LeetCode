f1865964e9caa466275c1a67683a3f1751507c9d
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-04 13:55:22 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int mirrorReflection(int p, int q) {
-        return (p%2==0 && q%2==0)?mirrorReflection(p/2,q/2):(1- p%2 + q%2);
+    int mirrorReflection(int &p, int &q) {
+        return (p&1) || (q&1)?1- (p&1) + (q&1):mirrorReflection(p>>=1,q>>=1);
     }
 };
