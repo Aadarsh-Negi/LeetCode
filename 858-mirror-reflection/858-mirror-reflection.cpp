c83df5675a9b24959223386b76fb9fcdedefb77a
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-04 13:51:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     int mirrorReflection(int p, int q) {
         while(!(p&1) && !(q&1)) p>>=1,q>>=1;
-        return 1 - p%2 + q%2;
+        return 1 - (p&1) + (q&1);
     }
 };
