38bc314758b5fe6249268ea3363bb1d8d7e46a80
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-05 14:52:40 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,18 +1,28 @@
-int dp[1004];
+
 class Solution {
 public:
-    int solve(vector<int> &ar,int tar){
-        if(tar==0) return 1;
-        if(dp[tar]!=-1) return dp[tar];
+//     int solve(vector<int> &ar,int tar){
+//         if(tar==0) return 1;
+//         if(dp[tar]!=-1) return dp[tar];
         
-        int temp=0;
-        for(int i=0;i<ar.size();i++){
-            if(ar[i] <= tar) temp+=solve(ar,tar-ar[i]);
-        }
-        return dp[tar] = temp;
-    }
+//         int temp=0;
+//         for(int i=0;i<ar.size();i++){
+//             if(ar[i] <= tar) temp+=solve(ar,tar-ar[i]);
+//         }
+//         return dp[tar] = temp;
+//     }
     int combinationSum4(vector<int>& ar, int target) {
-        memset(dp,-1,sizeof(dp));
-        return solve(ar,target);
+        unsigned int dp[target+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        
+        for(int i=1;i<=target;i++)
+            for(int &j:ar)
+                if(i>=j) dp[i]+=dp[i-j];
+        
+        
+        
+        
+        return dp[target];
     }
 };
