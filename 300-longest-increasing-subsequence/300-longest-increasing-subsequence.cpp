d9af1883944160bc9e6f11f018c75aaf68b07dfd
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-08 09:29:35 +0530

    Time: 8 ms (95.75%), Space: 10.4 MB (88.72%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,24 +1,15 @@
-int dp[2600];
+
 class Solution {
 public:
-    int solve(int i,vector<int> &ar,int mn = INT_MIN){
-        if(i==ar.size()) return 0;
-        
-        
-        int temp = -1;
-        if(ar[i]>mn){
-            if(dp[i]==-1) dp[i] = 1+solve(i+1,ar,ar[i]);
-            temp = dp[i];
-        }
-        temp = max(temp,solve(i+1,ar,mn));
-        
-        return temp;
-    }
+
     
-    int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        // ans=-1;
-        return solve(0,nums);
-        // return ans;
+int lengthOfLIS(vector<int>& a) {
+     vector<int> lis;
+     for (int i = 0; i < a.size(); i++) {
+        auto it = lower_bound(lis.begin(), lis.end(), a[i]);
+        if (it != lis.end()) *it = a[i];
+        else lis.push_back(a[i]);
     }
+    return lis.size();
+}
 };
