228b4c7b991e7365d8402d8ab7c9f8a7a54f83ec
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-19 14:40:53 +0530

    Time: 213 ms (25.74%), Space: 59.1 MB (20.90%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& ar) {
+       map<int,int> lf;
+       map<int,int> ed;
+        
+       for(int i:ar) lf[i]++;
+       
+        for(int i:ar){
+            if(!lf[i]) continue;
+            lf[i]--;
+            if(ed[i-1]){
+                ed[i-1]--;
+                ed[i]++;
+            }else if(lf[i+1] && lf[i+2]){
+                ed[i+2]++;
+                lf[i+1]--;
+                lf[i+2]--;
+            }else return false;
+            
+        }
+    return true;
+        
+    }
+};
