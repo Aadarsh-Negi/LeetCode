758b07eb9cf1b78aa15ce4c478494397f4fe4905
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-21 03:22:39 +0530

    Time: 6 ms (78.11%), Space: 9.1 MB (27.35%) - LeetHub

---
+++ 942-di-string-match/942-di-string-match.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> ans;
+        int f=0;
+        int l=s.size();
+        
+        
+        for(char &c:s){
+            if(c=='I') ans.push_back(f++);
+            else ans.push_back(l--);    
+        }
+        ans.push_back(f);
+        return ans;
+    }
+};
