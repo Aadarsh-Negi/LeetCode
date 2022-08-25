22331296b35c23362c5953cfd6dfa8cd3dee0f7f
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-08-25 12:00:46 +0530

    Time: 1637 ms (62.44%), Space: 17.2 MB (31.11%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,16 +1,17 @@
 int dp[105][10004];
+int ar[105][105];
 class Solution {
 public:
     int n,tx;
-    
-    int solve(vector<vector<int>> &mat,int i,int ttl=0){
+    int m;
+    int solve(int i,int ttl=0){
         if(i>=n) return abs(tx - ttl);
             
         if(dp[i][ttl]!=-1) return dp[i][ttl];
         int temp = INT_MAX;
-        for(int j=0;j<mat[i].size();j++){
-            temp = min(temp,solve(mat,i+1,ttl+mat[i][j]));
-            if(temp==0) break;
+        for(int j=0;j<m;j++){
+            temp = min(temp,solve(i+1,ttl+ar[i][j]));
+            // if(temp==0) break;
         }
         return dp[i][ttl] = temp;
     }
@@ -18,9 +19,12 @@ public:
     
     int minimizeTheDifference(vector<vector<int>>& mat, int tar) {
         n=mat.size();
+        m = mat[0].size();
+        for(int i=0;i<n;i++)
+            for(int j=0;j<mat[i].size();j++) ar[i][j] = mat[i][j];
         tx=tar;
         memset(dp,-1,sizeof(dp));
-        return solve(mat,0);
+        return solve(0);
 
     }
 };
