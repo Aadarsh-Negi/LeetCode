b7b4334b91cd980b5ae77df8bec99f6a05d36cfe
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-09-01 10:53:50 +0530

    Time: 3 ms (80.57%), Space: 7.1 MB (29.75%) - LeetHub

---
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int uniquePathsIII(vector<vector<int>>& gg) {
+        int free=0;
+        int n = gg.size();
+        int m = gg[0].size();
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                free+=(gg[i][j]==0);
+        
+        int ans=0;
+        
+        function<void(int,int,int)> solve = [&](int i,int j,int free){
+            if(i<0 || j<0 || i>=n || j>=m || gg[i][j]==3 || gg[i][j]==-1) return;
+            if(gg[i][j]==2){
+                if(free==0) ans++;
+                return;
+            }
+            
+            gg[i][j] = 3;
+            solve(i+1,j,free-1);
+            solve(i,j+1,free-1);
+            solve(i-1,j,free-1);
+            solve(i,j-1,free-1);
+            gg[i][j] = 1;
+            
+        };
+        
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(gg[i][j]==1){ 
+                    // cout<<i<<" "<<j<<"\n";
+                    solve(i,j,free+1); 
+                    return ans;
+                }
+        
+        return ans;
+    }
+};
