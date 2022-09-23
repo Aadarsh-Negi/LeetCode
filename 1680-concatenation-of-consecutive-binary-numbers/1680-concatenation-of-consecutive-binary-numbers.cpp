95eba023a51c50c92bc68b1ab1921ba0ec763dbd
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-09-23 10:10:24 +0530

    Time: 77 ms (70.11%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -4,17 +4,14 @@ public:
         
         long res = 0;
         int const mod = 1e9 + 7;
-        
         for(int i=1;i<=n;i++){
-            int len=0;
-            while((1<<len)<=i) len++;
-            res<<=len;
+            int val = i;
+            while(val) val>>=1,res<<=1;
             res%=mod;
             res+=i;
             res%=mod;
         }
         
         return res;
-        
     }
 };
