5aeb633a37ab8881b348cc54f9b18d492b3e95ea
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-09-23 10:22:03 +0530

    Time: 2577 ms (5.43%), Space: 334.5 MB (6.52%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    int concatenatedBinary(int n) {
-        
-        long res = 0;
-        int const mod = 1e9 + 7;
-        for(int i=1;i<=n;i++){
-            int val = i;
-            while(val) val>>=1,res<<=1;
-            res%=mod;
-            res+=i;
-            res%=mod;
+    string bin(int x){
+        string s;
+        while(x){
+            s+=(x%2?'1':'0');
+            x/=2;
         }
-        
+        return s;
+    }
+    int concatenatedBinary(int n) {
+        string ans;
+        while(n){
+            ans+=bin(n);
+            n--;
+        }
+        long long res = 0;
+        int const mod = 1e9 + 7;
+        long long p=1;
+        for(char &c:ans){
+            if(c=='1'){
+                res+=p;
+                res%=mod;
+            }
+            p*=2;
+            p%=mod;
+        }
         return res;
+        
     }
 };
