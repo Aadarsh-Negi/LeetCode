6740bed5c7813e1e6c3575962a50c6aa3cab3dbd
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-10-09 10:44:27 +0530

    Time: 85 ms (39.59%), Space: 47.1 MB (5.10%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,13 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+int fq[55555];
+int const N = 1e4;
 class Solution {
 public:
     unordered_map<int,int> fq;
     bool findTarget(TreeNode* root, int k) {
         if(!root) return 0;
-        if(fq[k-root->val]) return 1;
-        fq[root->val]++;
+        int val = k-root->val + N;
+        if(fq[val]) return 1;
+        fq[root->val+N]++;
         return findTarget(root->left,k)|findTarget(root->right,k);
     }
 };
