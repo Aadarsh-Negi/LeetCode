6f73a49dc98a135d4c6f95978ce74d34ffc78bf8
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-10-17 11:11:04 +0530

    Time: 3 ms (80.43%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -3,8 +3,7 @@ public:
     bool checkIfPangram(string c) {
         int cnt[26]={0};
         for(char &s:c) cnt[s-'a']++;
-        bool ok = 1;
-        for(int i=0;i<26;i++) ok&=cnt[i]>=1;
-        return ok;
+        for(int i=0;i<26;i++) if(!cnt[i]) return false;
+        return true;
     }
 };
