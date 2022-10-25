9b8e45f5d402667d4e289d45abbfc4cbcd6f788d
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-10-25 19:50:18 +0530

    Time: 4 ms (95.91%), Space: 8.2 MB (92.71%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int maxLength(vector<string>& ar) {
-        int ans = 0;
-        int n = ar.size();
-        for(int i=0;i<(1<<n);i++){
-            int cnt[27] = {0};
-            bool ok=1;
-            int temp=0;
-            for(int j=0;j<n && ok;j++){
-                if((1<<j)&i){
-                    for(char &c:ar[j]){
-                        cnt[c-'a']++;
-                        temp++;
-                        if(cnt[c-'a']>1){
-                            ok=0;
-                            break;
-                        }
-                    }    
-                }
-            }
-            if(ok) ans = max(ans,temp);
-        }
-        
-        return ans;
-        
+    int n ,ans = 0;
+    bool isvalid( string&s, int &mask) {
+       for(auto it : s) {
+          if(mask&(1<<(it-'a'))) return false;
+          mask = mask|(1<<(it-'a'));
+       }
+      return true;
+    }
+    void solve(vector<string>&a , int i , int mask , int temp) {
+       ans = max(ans , temp);
+       if(i>=n) return;
+       
+          int tmask = mask;
+          if(isvalid(a[i],tmask)) 
+            solve(a,i+1,tmask,temp+a[i].size());
+          
+             solve(a,i+1,mask,temp);
+       
+    }
+    int maxLength(vector<string>& a) {
+         n = a.size();
+        solve(a,0,0,0);
+       return ans;
     }
 };
