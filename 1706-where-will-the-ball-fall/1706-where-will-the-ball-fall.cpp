d9c2d616a6b10544a9cff72a04bc75a16294ec6c
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-11-01 15:31:36 +0530

    Time: 32 ms (82.25%), Space: 13.1 MB (80.92%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -3,16 +3,16 @@ public:
    
     vector<int> findBall(vector<vector<int>>& gg){
         vector<int> ans;
-        auto solve = [&](auto &&solve,int i,int j){
+        function<int(int,int)> solve = [&](int i,int j){
             if(i==gg.size()) return j;
             int j2 = j + gg[i][j];
             if(j2<0 || j2>=gg[0].size() || gg[i][j]!=gg[i][j2])
                 return -1;
-            return solve(solve,i+1,j2);
+            return solve(i+1,j2);
         };
     
         for(int i=0;i<gg[0].size();i++)
-            ans.push_back(solve(solve,0,i));
+            ans.push_back(solve(0,i));
         return ans;
     }
 };
