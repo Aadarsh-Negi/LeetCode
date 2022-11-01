78fba2d9fe4e1f5a96009a9f50c2e76982433688
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-11-01 15:25:55 +0530

    Time: 61 ms (34.61%), Space: 13.2 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>> &gg){
-        if(i==gg.size()) return j;
-        int j2 = j + gg[i][j];
-        if(j2<0 || j2>=gg[0].size() || gg[i][j]!=gg[i][j2])
-            return -1;
-        
-        return solve(i+1,j2,gg);
-    }
-    vector<int> findBall(vector<vector<int>>& gg) {
-        int n = gg.size();
-        int m = gg[0].size();
-        vector<int> ans(m);
-        for(int i=0;i<m;i++){
-            ans[i] = solve(0,i,gg);
-        }
-            
-        
+   
+    vector<int> findBall(vector<vector<int>>& gg){
+        vector<int> ans;
+        auto solve = [&](auto &&solve,int i,int j){
+            if(i==gg.size()) return j;
+            int j2 = j + gg[i][j];
+            if(j2<0 || j2>=gg[0].size() || gg[i][j]!=gg[i][j2])
+                return -1;
+            return solve(solve,i+1,j2);
+        };
+    
+        for(int i=0;i<gg[0].size();i++)
+            ans.push_back(solve(solve,0,i));
         return ans;
     }
 };
