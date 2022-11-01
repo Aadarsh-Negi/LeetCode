e1817db4f71333fd0d005f4eedb63239f11369d8
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-11-01 15:33:34 +0530

    Time: 49 ms (63.64%), Space: 13.3 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -5,10 +5,8 @@ public:
         vector<int> ans;
         function<int(int,int)> solve = [&](int i,int j){
             if(i==gg.size()) return j;
-            int j2 = j + gg[i][j];
-            if(j2<0 || j2>=gg[0].size() || gg[i][j]!=gg[i][j2])
-                return -1;
-            return solve(i+1,j2);
+            if(j+gg[i][j]<0 || j+gg[i][j]>=gg[0].size() || gg[i][j]!=gg[i][j+gg[i][j]])return -1;
+            return solve(i+1,j+gg[i][j]);
         };
     
         for(int i=0;i<gg[0].size();i++)
