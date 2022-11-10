b4f239370c6b9b0182447df249946aa42afb923e
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-11-10 18:53:01 +0530

    Time: 518 ms (30.77%), Space: 120.9 MB (42.42%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+       vector<int> ans;
+        
+        for(int i:nums){
+                int c = i;
+                while(ans.size() && gcd(ans.back(),c)>1){
+                    c = lcm(ans.back(),c);
+                    ans.pop_back();
+                }
+            
+                ans.push_back(c);                
+            
+        }
+        return ans;
+    }
+};
