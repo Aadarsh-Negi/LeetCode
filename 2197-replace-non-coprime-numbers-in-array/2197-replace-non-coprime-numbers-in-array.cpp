3620740390c6ea02cf6f49b2ba74684144bf04d7
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-11-10 18:56:56 +0530

    Time: 539 ms (22.85%), Space: 120.9 MB (42.42%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(st.empty())
-            {
-                st.push(nums[i]);
-            }
-             else if(__gcd(st.top(),nums[i]) == 1)
-            {
-                st.push(nums[i]);
-            }
-            else
-            {
-                int x = nums[i];
-                while(st.size() && __gcd(x,st.top())>1){
-                    x = lcm(st.top(),x);
-                    st.pop();
-                }
-                st.push(x);
-            }
-        }
-        
         vector<int> ans;
-        while(!st.empty())
-        {
-            int x = st.top();
-            ans.push_back(x);
-            st.pop();
-        }
         
-        reverse(ans.begin(),ans.end());
+        for(int i:nums){
+                int c = i;
+                while(ans.size() && gcd(ans.back(),c)>1){
+                    c = lcm(ans.back(),c);
+                    ans.pop_back();
+                }
+                ans.push_back(c);                
+        }
         return ans;
     }
 };
