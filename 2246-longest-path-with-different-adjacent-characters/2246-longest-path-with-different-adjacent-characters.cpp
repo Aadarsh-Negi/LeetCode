63d5852db14c8e5f5597289803fbc22b60687d55
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-01-14 00:18:46 +0530

    Time: 903 ms (40.83%), Space: 254.2 MB (10.51%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -8,14 +8,9 @@ public:
         for(int i:gg[u]){
             int x = solve(i,s);
             if(s[u]==s[i])  continue;
-            
-            
-            // if(sub.size()<2) sub.push_back(x);
-            // else{
-                sub.push_back(x);
-                sort(sub.rbegin(),sub.rend());
-                sub.pop_back();
-            // }
+            sub.push_back(x);
+            sort(sub.rbegin(),sub.rend());
+            sub.pop_back();
         }
         ans=max(ans,sub[0]+sub[1]+1); 
         return sub[0]+1;
@@ -24,11 +19,10 @@ public:
     int longestPath(vector<int>& par, string s) {
         gg.resize(par.size()+10);
         
-        int root=0;
         ans=0;
         for(int i=1;i<par.size();i++) gg[par[i]].push_back(i);
         
-        solve(root,s);
+        solve(0,s);
         return ans;
     }
 };
