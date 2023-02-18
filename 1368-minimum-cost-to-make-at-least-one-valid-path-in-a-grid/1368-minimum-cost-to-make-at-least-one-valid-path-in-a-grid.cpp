657f61ed6718d08e84da2fa321b27581c981afba
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-02-18 19:50:30 +0530

    Time: 123 ms (44.68%), Space: 21 MB (42.32%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -7,11 +7,11 @@ public:
         const int m = g[0].size();
         queue<pair<int,int>> qq;
         qq.push({0,0});
-        vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
-        dis[0][0] = 0;
+        vector<vector<int>> distance(n,vector<int>(m,INT_MAX));
+        distance[0][0] = 0;
         while(qq.size()){
-            int sz = qq.size();
-            while(sz--){
+            // int sz = qq.size();
+            // while(sz--){
                 int i = qq.front().first;
                 int j = qq.front().second;
                 qq.pop();
@@ -20,13 +20,13 @@ public:
                     int x = i+dx[k];
                     int y = j+dy[k];
                     if(x<0 || y<0 || x>=n || y>=m) continue;
-                    if(dis[x][y] > dis[i][j] + (c!=k)){
-                        dis[x][y] = dis[i][j] + (c!=k);
+                    if(distance[x][y] > distance[i][j] + (c!=k)){
+                        distance[x][y] = distance[i][j] + (c!=k);
                         qq.push({x,y});
                     }
-                }
+                // }
             }
         }
-        return dis[n-1][m-1];
+        return distance[n-1][m-1];
     }
 };
