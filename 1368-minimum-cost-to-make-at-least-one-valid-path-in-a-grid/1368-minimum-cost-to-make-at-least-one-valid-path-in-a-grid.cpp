e1c3b88d23fd5afc3b368670715cddbe00ebf22d
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-02-18 19:41:49 +0530

    Time: 156 ms (33.81%), Space: 20.9 MB (43.74%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -3,8 +3,8 @@ int dy[] = {1,-1,0,0};
 class Solution {
 public:
     int minCost(vector<vector<int>>& g) {
-        int n = g.size();
-        int m = g[0].size();
+        const int n = g.size();
+        const int m = g[0].size();
         queue<pair<int,int>> qq;
         qq.push({0,0});
         vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
@@ -23,8 +23,7 @@ public:
                 for(int k=0;k<4;k++){
                     int x = i+dx[k];
                     int y = j+dy[k];
-                    if(x < 0 || y<0 || x>=n || y>=m) continue;
-                    
+                    if(x<0 || y<0 || x>=n || y>=m) continue;
                     int p = dis[x][y];
                     dis[x][y] = min(dis[x][y],dis[i][j] + (c!=k));
                     if(p!=dis[x][y]) qq.push({x,y});
