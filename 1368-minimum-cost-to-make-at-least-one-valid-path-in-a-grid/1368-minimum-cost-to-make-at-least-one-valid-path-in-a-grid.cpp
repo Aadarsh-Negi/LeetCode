bd43af618bb11c4e8807a0a3a68537e3268e01ca
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-02-19 19:20:23 +0530

    Time: 35 ms (94.58%), Space: 14.1 MB (72.88%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,26 +5,31 @@ public:
     int minCost(vector<vector<int>>& g) {
         const int n = g.size();
         const int m = g[0].size();
-        queue<pair<int,int>> qq;
-        qq.push({0,0});
-        vector<vector<int>> distance(n,vector<int>(m,INT_MAX));
+        deque<pair<int,int>> qq;
+        qq.push_back({0,0});
+        vector<vector<int>> distance(n,vector<int>(m,INT_MAX/2));
         distance[0][0] = 0;
         while(qq.size()){
-            int sz = qq.size();
-            while(sz--){
                 int i = qq.front().first;
                 int j = qq.front().second;
-                qq.pop();
+                qq.pop_front();
+                if(i==n-1 && j==m-1) return distance[i][j];
                 int c = g[i][j]-1;
                 for(int k=0;k<4;k++){
                     int x = i+dx[k];
                     int y = j+dy[k];
                     if(x<0 || y<0 || x>=n || y>=m) continue;
-                    if(distance[x][y] > distance[i][j] + (c!=k)){
-                        distance[x][y] = distance[i][j] + (c!=k);
-                        qq.push({x,y});
+                    if(c==k){
+                        if(distance[x][y] > distance[i][j]){
+                            distance[x][y] = distance[i][j];
+                            qq.push_front({x,y});
+                        }    
+                    }else{
+                        if(distance[x][y] > distance[i][j] + 1){
+                            distance[x][y] = distance[i][j] + 1;
+                            qq.push_back({x,y});
+                        }    
                     }
-                }
             }
         }
         return distance[n-1][m-1];
