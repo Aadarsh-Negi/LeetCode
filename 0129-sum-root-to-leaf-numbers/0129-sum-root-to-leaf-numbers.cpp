71a4dec04044b0210df8829c7155f4b223bc35d7
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-03-14 15:46:55 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (87.90%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,19 +11,10 @@
  */
 class Solution {
 public:
-int ans;
-void solve(TreeNode *root,int x = 0){
-    if(!root) return;
-    x*=10;
-    x+=root->val;
-    if(root->left == root->right) ans+=x;
-
-    solve(root->left,x);
-    solve(root->right,x);
-}
-    int sumNumbers(TreeNode* root) {
-        ans=0;
-        solve(root);
-        return ans;
+    int sumNumbers(TreeNode* root,int x=0) {
+        if(!root) return 0;
+        x = x*10 + root->val;
+        if(root->left == root->right) return x;
+        return sumNumbers(root->left,x) + sumNumbers(root->right,x);
     }
 };
