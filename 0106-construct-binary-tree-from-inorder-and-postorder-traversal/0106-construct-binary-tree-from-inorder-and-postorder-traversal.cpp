b4c32855fcd3ada592534076e67b773e4e257d8d
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-03-16 13:37:05 +0530

    Time: 16 ms (80.37%), Space: 26 MB (78.64%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,11 +1,12 @@
+int ss[6666];
 class Solution {
 public:
-    unordered_map<int,int> ss;
+    
     TreeNode* solve(vector<int> &p,int i,int j){
         if(i>j) return nullptr;
         
         TreeNode *temp = new TreeNode(p.back());
-        int mid=ss[p.back()];
+        int mid=ss[p.back()+3000];
         p.pop_back();
         
         // while(mid<in.size() && in[mid]!=p.back()) mid++;
@@ -17,8 +18,8 @@ public:
     }
     
     TreeNode* buildTree(vector<int>& in, vector<int>& post) {
-        for(int i=0;i<in.size();i++) ss[in[i]] = i;
-        
+        for(int i=0;i<in.size();i++) ss[in[i]+3000] = i;
+        // memset(ss,-1,sizeof(ss));
         return solve(post,0,in.size()-1);
         
         // return root;
