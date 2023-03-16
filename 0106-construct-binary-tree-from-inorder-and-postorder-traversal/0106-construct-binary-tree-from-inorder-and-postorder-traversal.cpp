13aaa2ff993167f5e8c5f82ce5d1cd97bbbfe643
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-03-16 13:49:32 +0530

    Time: 15 ms (84.81%), Space: 26.1 MB (78.64%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,28 +1,25 @@
 int ss[6666];
+int p[3333];
 class Solution {
 public:
-    
-    TreeNode* solve(vector<int> &p,int i,int j){
+    int k;
+    TreeNode* solve(int i,int j){
         if(i>j) return nullptr;
         
-        TreeNode *temp = new TreeNode(p.back());
-        int mid=ss[p.back()+3000];
-        p.pop_back();
-        
-        // while(mid<in.size() && in[mid]!=p.back()) mid++;
+        TreeNode *temp = new TreeNode(p[k]);
+        int mid=ss[p[k]+3000];
         // p.pop_back();
+        k--;
         
-        temp->right = solve(p,mid+1,j);
-        temp->left = solve(p,i,mid-1);
+        temp->right = solve(mid+1,j);
+        temp->left = solve(i,mid-1);
         return temp;
     }
     
     TreeNode* buildTree(vector<int>& in, vector<int>& post) {
         for(int i=0;i<in.size();i++) ss[in[i]+3000] = i;
-        // memset(ss,-1,sizeof(ss));
-        return solve(post,0,in.size()-1);
-        
-        // return root;
-        
+        for(int i=0;i<in.size();i++) p[i] = post[i];
+        k = in.size()-1;
+        return solve(0,in.size()-1);
     }
 };
