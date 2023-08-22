5d341f93d868775d98a40060750a19a49269a0b7
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-08-22 20:34:42 +0530

    Time: 309 ms (73.04%), Space: 221.1 MB (46.14%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
     int longestEqualSubarray(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<vector<int>> val(n+1);
-        unordered_map<int,int> prev;
-        for(int i=0;i<n;i++){
-            if(prev[nums[i]]){
-                int cc = i - prev[nums[i]];
-                val[nums[i]].push_back(cc);   
-            }
-            prev[nums[i]] = i+1;
+        int n=nums.size();
+        vector<vector<int>> ind(n+1);
+        for(int i=0;i<nums.size();i++){
+            ind[nums[i]].push_back(i);
         }
-        
-        int ans = 1;
-        
-        for(vector<int> &a:val){
-            for(int i=1;i<a.size();i++){
-                a[i]+=a[i-1];
-            }
-            auto ok = [&](int cnt,int K){
-                for(int i=cnt;i<a.size();i++){
-                    if(a[i] - (i-cnt-1>=0 ? a[i-cnt-1]:0) <= K) return true;
-                }
-                return false;
-                
-            };
-            
-            
-            int l = 1;
-            int r = a.size();
-            int temp = 0;
-            while(l<=r){
-                int mid  = l + (r-l)/2;
-                if(ok(mid-1,k)){
-                    temp = mid;
-                    l = mid+1;
-                }else r = mid-1;
-            }
-            ans=max(ans,1+temp);
+
+        int ans=1;
+        for(int v=1;v<=n;v++){
+           int cur = 1;
+           int last = 1;
+           int temp = 1;
+           int rem = k;
+           while(cur < ind[v].size()){
+               if(rem-(ind[v][cur] - ind[v][cur-1]-1)>=0){
+                 rem-=(ind[v][cur] - ind[v][cur-1]-1);
+                 temp++;
+                 cur++;
+               } 
+               else{
+                    while(rem-(ind[v][cur] - ind[v][cur-1]-1)<0 && last<=cur){
+                        rem+=ind[v][last] - ind[v][last-1]-1;
+                        temp--;
+                        last++;
+                    }
+                   if(last>cur){
+                       cur++;
+                       rem = k;
+                       temp=1;
+                   }
+               }
+               ans=max(ans,temp);
+           }
         }
-        
-        
         return ans;
     }
 };
