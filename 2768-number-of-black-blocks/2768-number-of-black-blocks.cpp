9dffc9fd9f2be2d3258415982279c62c50c5100d
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-08-27 22:06:10 +0530

    Time: 626 ms (98.35%), Space: 242.8 MB (79.84%) - LeetHub

--- 2768-number-of-black-blocks/2768-number-of-black-blocks.cpp
+++ 2768-number-of-black-blocks/2768-number-of-black-blocks.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<long long> countBlackBlocks(int n, int m, vector<vector<int>>& c) {
         vector<long long> ans(5,0);
-        
-        map<pair<int,int>,int> cp;
+        long l = 1e5 + 2;
+        unordered_map<int,int> cp;
         
         for(vector<int> &a:c){
             int x = a[0];
             int y = a[1];
-            if(x-1 >= 0 && y-1 >= 0) cp[{x-1,y-1}]++;
-            if(x+1 < n && y+1 < m) cp[{x,y}]++;
-            if(x-1 >= 0 && y+1 < m) cp[{x-1,y}]++;
-            if(x+1 < n && y-1 >= 0) cp[{x,y-1}]++;
+            if(x-1 >= 0 && y-1 >= 0) cp[l*(x-1) + y-1]++;
+            if(x+1 < n && y+1 < m) cp[l*x + y]++;
+            if(x-1 >= 0 && y+1 < m) cp[l*(x-1) + y]++;
+            if(x+1 < n && y-1 >= 0) cp[l*x+y-1]++;
         }
         
         for(auto it:cp) ans[it.second]++;
