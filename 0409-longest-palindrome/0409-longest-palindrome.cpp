b556c85916aa4022742a249a4b4185427fc1c3a1
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2023-11-05 15:45:43 +0530

    Time: 2 ms (63.24%), Space: 7 MB (64.17%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int ans = 0;
+        map<int,int> cnt;
+        
+        for(char &c:s) cnt[c]++;
+        
+        bool odd = 0;
+        for(auto it:cnt){
+            ans+=(it.second/2);
+            odd|=(it.second&1);
+        }
+        return ans*2 + odd;
+        
+    }
+};
