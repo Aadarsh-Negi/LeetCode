b89d81fa99b247ef4dd63cb7bbfbf160c7878d22
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2024-03-21 20:58:34 +0530

    Time: 22 ms (73.34%), Space: 94.7 MB (54.90%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,39 +4,35 @@ public:
         
         
         int n = x.size() + y.size();
-        
         int f = (n-1)/2;
         int s = n/2;
         f+=1;
         s+=1;
-        // cout<<f<<" <> "<<s<<"\n";
-        auto find = [&](int v,vector<int> &x,vector<int> &y)->int{
+        auto find = [&](int v)->int{
             int l = 0;
             int r = (int)x.size()-1;
             int ans = INT_MAX;
             while(l<=r){
                 int mid = l + (r-l)/2;
-                
-                int fx = upper_bound(y.begin(),y.end(),x[mid]) - y.begin(); // > x[mid]
-                
+                int fx = upper_bound(y.begin(),y.end(),x[mid]) - y.begin(); // > x[mid] 
                 if(fx + mid + 1 >= v){
-                    // cout<<fx<<" "<<mid<<" ><><>\n";
                     ans = x[mid];
                     r = mid-1;
                 }else l = mid+1;
-                
             }  
             return ans; 
         };
-        double a = min(find(f,x,y),find(f,y,x));
-        double b = min(find(s,x,y),find(s,y,x));
-       
+        int a = find(f);
+        int b = find(s);
+        
+        swap(x,y);
+        
+        a = min(a,find(f));
+        b = min(b,find(s));
         
         
-        // cout<<a<<" "<<b<<"\n";
-        
-        a += b;
-        a/=2.0;
-        return a;  
+        double res = a+b;
+        res/=2.0;
+        return res;  
     }
 };
